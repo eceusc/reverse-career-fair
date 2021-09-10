1da62e855070566d839a620ba7753fc536ce2e3b
Branson <bbeihl@ucsd.edu>
2021-09-10 00:23:00 -0700

    Added about section, no carousel function

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,5 +1,53 @@
+import {ReactComponent as Carousel} from '../../media/images/screen-interface/screen.svg'
+import {ReactComponent as Right} from '../../media/images/screen-interface/right-arrow.svg'
+import {ReactComponent as Left} from '../../media/images/screen-interface/left-arrow.svg'
+import {ReactComponent as Dim} from '../../media/images/screen-interface/dim-light.svg'
+import {ReactComponent as Lit} from '../../media/images/screen-interface/light.svg'
+import '../../css/about.css'
 export default function About(props){
     return (
-        <div>about</div>
+        <div className='about-page'>
+            <div className='text'>
+                <div className='title'><b>What is a reverse career fair?</b></div>
+                <div className='body'>
+                    The Reverse Career Fair is our initiative to help students find career opportunities by spotlighting hands-on project building experience.
+                    <br></br><br></br>
+                    At the Reverse Career Fair, students have the opportunity to present their projects and research to company representatives, 
+                    where they can first handedly display their achievements and talents to prospective employers 
+                    rather than showing just a few lines of text on their resume or a short introduction.
+                </div>
+            </div>
+            <div className='monitor-carousel'>
+                <div className='monitor'>
+                    <div className='board'>
+                        <Carousel/>
+                    </div>
+                    {/* <div className='screen'>
+                        <Carousel/>
+                    </div> */}
+                    <div className='left'>
+                        <Left/>
+                    </div>
+                    <div className='light-1'>
+                        <Lit/>
+                    </div>
+                    <div className='light-2'>
+                        <Dim/>
+                    </div>
+                    <div className='light-3'>
+                        <Dim/>
+                    </div>
+                    <div className='light-4'>
+                        <Dim/>
+                    </div>
+                    <div className='light-5'>
+                        <Dim/>
+                    </div>
+                    <div className='right'>
+                        <Right/>
+                    </div>
+                </div>
+            </div>
+        </div>
     ) 
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,7 @@
 import ArduinoWindow from "../../components/arduino_window"
 import Hero from "../../components/hero"
+import About from "../about"
+
 export default function Home(props){
     return (
         <>
@@ -12,13 +14,8 @@ export default function Home(props){
             >
                 <p>greetings </p>
             </Hero>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum repudiandae praesentium, deserunt odit, cupiditate id vel perferendis, architecto eos voluptas molestias tenetur atque voluptatem quas ducimus fugit accusamus exercitationem dolores ipsum. Assumenda soluta velit rerum nihil ut autem expedita minus, accusamus quasi molestiae reprehenderit officia natus vitae ipsa, nisi voluptate, corrupti commodi consequuntur eaque non doloribus? Esse, eum suscipit porro expedita facere quia quam illo et nostrum eius voluptate, dicta unde sequi ipsum dolor accusantium at iure. Repudiandae, earum unde? Blanditiis ex maiores autem. Ipsum saepe quaerat recusandae eveniet quis inventore, provident debitis qui eos voluptatibus modi, voluptate tempore quos?
-                </p>
-                <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum repudiandae praesentium, deserunt odit, cupiditate id vel perferendis, architecto eos voluptas molestias tenetur atque voluptatem quas ducimus fugit accusamus exercitationem dolores ipsum. Assumenda soluta velit rerum nihil ut autem expedita minus, accusamus quasi molestiae reprehenderit officia natus vitae ipsa, nisi voluptate, corrupti commodi consequuntur eaque non doloribus? Esse, eum suscipit porro expedita facere quia quam illo et nostrum eius voluptate, dicta unde sequi ipsum dolor accusantium at iure. Repudiandae, earum unde? Blanditiis ex maiores autem. Ipsum saepe quaerat recusandae eveniet quis inventore, provident debitis qui eos voluptatibus modi, voluptate tempore quos?
-                </p>
-            <div>home</div>
+            <About id='about'/>
+            <br></br>
             <ArduinoWindow id='faq'/>
             
         </>
