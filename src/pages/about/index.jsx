a287d1856ae4933621159f35521770479313ce1e
Branson <bbeihl@ucsd.edu>
2021-09-11 20:00:31 -0700

    Carousel function without pictures

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -4,7 +4,40 @@ import {ReactComponent as Left} from '../../media/images/screen-interface/left-a
 import {ReactComponent as Dim} from '../../media/images/screen-interface/dim-light.svg'
 import {ReactComponent as Lit} from '../../media/images/screen-interface/light.svg'
 import '../../css/about.css'
+import { useState } from 'react'
+
+function RenderScreen(props) {
+    console.log(props.index);
+    switch (props.index) {
+        case 1:
+            return <svg width="500" height="500" viewBox="0 0 286 288" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M274 49H12V188H274V49Z" fill="#FFFFFF"/>
+            </svg>;
+        default:
+            return <svg width="500" height="500" viewBox="0 0 286 288" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M274 49H12V188H274V49Z" fill="#C4C4C4"/>
+            </svg>;
+    }
+}
+
+function Render1(props) { 
+    if (props.index === 0) {return <Lit/>;} else {return <Dim/>;}
+}
+function Render2(props) {
+    if (props.index === 1) {return <Lit/>;} else {return <Dim/>;}
+}
+function Render3(props) {
+    if (props.index === 2) {return <Lit/>;} else {return <Dim/>;}
+}
+function Render4(props) {
+    if (props.index === 3) {return <Lit/>;} else {return <Dim/>;}
+}
+function Render5(props) {
+    if (props.index === 4) {return <Lit/>;} else {return <Dim/>;}
+}
+
 export default function About(props){
+    const [index, setState] = useState(0);
     return (
         <div className='about-page'>
             <div className='text'>
@@ -20,31 +53,35 @@ export default function About(props){
             <div className='monitor-carousel'>
                 <div className='monitor'>
                     <div className='board'>
-                        <Carousel/>
+                       <Carousel/>
                     </div>
-                    {/* <div className='screen'>
-                        <Carousel/>
-                    </div> */}
+                    <div className='screen'>
+                        <RenderScreen index={index}/>
+                    </div>
                     <div className='left'>
-                        <Left/>
+                        <button className='l-but' onClick={() => setState(index => ((index - 1) % 5) < 0 ? 4 : (index - 1) % 5)}>
+                            <Left/>
+                        </button>
                     </div>
                     <div className='light-1'>
-                        <Lit/>
+                        <Render1 index={index} />
                     </div>
                     <div className='light-2'>
-                        <Dim/>
+                        <Render2 index={index} />
                     </div>
                     <div className='light-3'>
-                        <Dim/>
+                        <Render3 index={index} />
                     </div>
                     <div className='light-4'>
-                        <Dim/>
+                        <Render4 index={index} />
                     </div>
                     <div className='light-5'>
-                        <Dim/>
+                        <Render5 index={index} />
                     </div>
                     <div className='right'>
-                        <Right/>
+                        <button className='r-but' onClick={() => setState(index => (index + 1) % 5)}>
+                            <Right/>
+                        </button>
                     </div>
                 </div>
             </div>
