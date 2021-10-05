633dbaf0860b6a3d149a355df5efd1a07436f368
Branson <bbeihl@ucsd.edu>
2021-10-04 17:30:58 -0700

    added hero

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,6 +4,14 @@ import About from "../../components/about"
 import Companies from "../../components/companies"
 import Apply from "../../components/apply"
 import Footer from "../../components/footer"
+import {ReactComponent as HeroImg} from "../../media/images/Hero_img.svg"
+import {ReactComponent as Btn} from '../../media/images/menu button white.svg';
+
+function Btnclk (props) {
+    console.log("1");
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    return 1;
+}
 
 export default function Home(props){
     return (
@@ -11,7 +19,15 @@ export default function Home(props){
             <Hero
             className="home"
             >
-                <p>greetings </p>
+                <div className="text-button">
+                    Showcase your projects. <br></br>
+                    Join ECE Reverse Career fair!<br></br>
+                    
+                    <button className="button" onClick={Btnclk}><Btn className="hero-button"/>  </button>
+                </div>
+                <div className="hero-image">
+                    <HeroImg/>
+                </div>
             </Hero>
             <About id='about'/>
             <br></br>
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,5 +1,21 @@
+import '../../css/projects.css'
+
 export default function Projects(props){
     return (
-        <div>projects</div>
+        <div className="project-showcase">
+            <div className="title"> 
+                Project Showcase
+            </div>
+            <div className="subtext">
+                Subtext
+            </div>
+            <div className="project-grid">
+                <div className="grid-row">
+                    <div className="project-container">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     ) 
 }
